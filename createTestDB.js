ed171a190166ed1f74007f3f88f1d9a6117b262a
Aaron Butterworth <aaron.david.butterworth@gmail.com>
2018-04-10 13:33:23 +0100

    Added basic user table

--- createTestDB.js
+++ createTestDB.js
@@ -7,38 +7,38 @@ var con = mysql.createConnection({
 	password: process.env.MYSQL 
 });
 
-con.connect(function(err) {
+con.query("DROP DATABASE pubTestDB", function (err, result) {
+ if (err) throw err;
+	console.log("Database dropped");
+});
+
+con.query("CREATE DATABASE pubTestDB", function (err, result) {
 	if (err) throw err;
-	console.log("Connected!");
+	console.log("Database created");
+});
 
-	con.query("DROP DATABASE pubTestDB", function (err, result) {
-	 if (err) throw err;
-		console.log("Database dropped");
-	});
+con.query("USE pubTestDB", function (err, result) {
+	if (err) throw err;
+	console.log("Database selected");
+});
 
-	con.query("CREATE DATABASE pubTestDB", function (err, result) {
+con.query("CREATE TABLE pubs (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(40), description TEXT, url VARCHAR(15) NOT NULL)", function (err, result) {
+	if (err) throw err;
+	console.log("Table \"Pubs\" created");
+});
+
+con.query("CREATE TABLE users (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(40) NOT NULL, password VARCHAR(255) NOT NULL)", function (err, result) {
+	if (err) throw err;
+	console.log("Table \"Users\" created");
+});
+
+fs.readFile(__dirname + "/testData/pubs.sql", "utf8", function (err,data) {
+	if (err) {
+		return console.log(err);
+	}
+	con.query(data, function (err, result) {
 		if (err) throw err;
-		console.log("Database created");
-	});
-
-	con.query("USE pubTestDB", function (err, result) {
-		if (err) throw err;
-		console.log("Database selected");
-	});
-
-	con.query("CREATE TABLE pubs (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(40), description TEXT, url VARCHAR(15))", function (err, result) {
-		if (err) throw err;
-		console.log("Table \"Pubs\" created");
-	});
-
-	fs.readFile(__dirname + "/testData/pubs.sql", "utf8", function (err,data) {
-		if (err) {
-			return console.log(err);
-		}
-		con.query(data, function (err, result) {
-			if (err) throw err;
-			console.log("Table \"Pubs\" populated");
-		});
+		console.log("Table \"Pubs\" populated");
 
 		con.end();
 	});
