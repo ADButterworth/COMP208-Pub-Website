980d102c99df6507d4d6a58ffabc4b561b036ed6
Aaron Butterworth <aaron.david.butterworth@gmail.com>
2018-04-10 13:35:18 +0100

    Added bcrypt, added error msg on failed pub addition

--- routes/addPub.js
+++ routes/addPub.js
@@ -13,26 +13,30 @@ var con = mysql.createConnection({
 
 // /addpub should redirect to home
 router.get('/', function (req, res) {
-	res.render('addPub');
+	res.render('addPub', {error: false});
 });
 
+// POST to /addpub sanitise/verify and insert into db
 router.post('/', function (req, res) {
 	var sanitisedURL = con.escape(req.body.url);
-	var sql = "SELECT * FROM pubs WHERE url = " + sanitisedURL;
 
+	// check if url in use
+	var sql = "SELECT * FROM pubs WHERE url = " + sanitisedURL;
 	con.query(sql, function(error, result, field) {
 		if (result.length == 0) {
 			var sanitisedName = con.escape(req.body.name);
 			var sanitisedDes = con.escape(req.body.description);
 
+			// name not in use, insert requested data into db
 			var sql = "INSERT INTO pubs(name, description, url) VALUES (" + sanitisedName + ", " + sanitisedDes + ", " + sanitisedURL + ")";
-
 			con.query(sql, function(error, result, field) {
-				res.redirect('../../pubs/' + req.body.url);
+
+				// redirect to new pages
+				res.redirect('../pubs/' + req.body.url);
 			});
 		}
 		else {
-			res.send("Error: URL Already Exists");
+			res.render('addPub', {error: true});
 		}
 	});
 });
