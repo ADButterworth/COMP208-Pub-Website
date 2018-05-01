36238a708454a72be26c0cc6ae41459fa250e03e
Aaron Butterworth <aaron.david.butterworth@gmail.com>
2018-05-01 15:22:21 +0100

    Fixed SQL for more recent versions

--- createTestDB.js
+++ createTestDB.js
@@ -27,7 +27,7 @@ con.query("CREATE TABLE pubs (id INT AUTO_INCREMENT PRIMARY KEY, ownerID INT NOT
 	console.log("    Table \"Pubs\" created");
 });
 
-con.query("CREATE TABLE users (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(40) NOT NULL, email VARCHAR(256) NOT NULL, name VARCHAR(40), password VARCHAR(255) NOT NULL, admin TINYINT(1) NOT NULL DEFAULT 0)", function (err, result) {
+con.query("CREATE TABLE users (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(40) NOT NULL, email VARCHAR(256) NOT NULL, name VARCHAR(40), password VARCHAR(255) NOT NULL, admin TINYINT(1) NOT NULL)", function (err, result) {
 	if (err) throw err;
 	console.log("    Table \"Users\" created");
 });
