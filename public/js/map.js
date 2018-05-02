f9a96338005630708a62bc166d7997fb231aad65
HarveyDWilliams <36428594+HarveyDWilliams@users.noreply.github.com>
2018-05-02 18:29:16 +0100

    Fixed pub maps scrolling

--- public/js/map.js
+++ public/js/map.js
@@ -1,6 +1,6 @@
 function initMap() { 
 	var script_tag = document.getElementById('loc')
 	var location = {lat: parseFloat(script_tag.getAttribute("lat")), lng: parseFloat(script_tag.getAttribute("lng"))}; 
-	var map = new google.maps.Map(document.getElementById('map'), { zoom: 15, center: location }); 
+	var map = new google.maps.Map(document.getElementById('map'), { zoom: 15, center: location, gestureHandling: 'none' }); 
 	var marker = new google.maps.Marker({ position: location, map: map }); 
 }
