189767e8521dddc5b2c0d70aa6ae6de072fa5b74
HarveyDWilliams <36428594+HarveyDWilliams@users.noreply.github.com>
2018-05-02 17:55:10 +0100

    Delete user now working
    An essential feature for when users realised what they've signed up to

--- routes/user.js
+++ routes/user.js
@@ -22,7 +22,7 @@ router.get('/:username', function(req, res) {
 			if (req.session.username == req.params['username']) {
 				var sql2 = 'SELECT DISTINCT * FROM pubs WHERE ownerID = ' + result[0].id;
 				con.query(sql2, function(error, result2, field) {
-					res.render('profile', {name: result[0].name, email: result[0].email, imgPath: "../img/" + result[0].imageName, pubs: result2, username: req.session.username, admin: req.session.admin});
+					res.render('profile', {name: result[0].name, email: result[0].email, imgPath: "../img/" + result[0].imageName, pubs: result2, username: req.session.username, admin: req.session.admin, owner: true});
 				});
 			}
 			else {
@@ -34,6 +34,16 @@ router.get('/:username', function(req, res) {
 			res.redirect('../../404');
 		}
 	});
+});
+
+router.post('/', function (req, res) {
+	var sql = 'DELETE FROM users WHERE username=\"' +req.session.username + "\""
+	con.query(sql, function(error, result2, field){
+		if (error) {
+			console.log(error)
+		}
+	});
+	res.redirect('./logout');
 });
 
 // last
